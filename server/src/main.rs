e67a56de6748d227c9e5988de24311057fdd7be3
Leslie-John Richardson <lesliejohnrichardson@gmail.com>
2025-12-27 18:00:25 +0100

    Update main.rs

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,6 +2,7 @@ use tokio::net::UnixListener;
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use std::path::Path;
 use std::fs;
+use std::os::unix::fs::PermissionsExt;
 
 #[tokio::main]
 async fn main() {
@@ -13,6 +14,8 @@ async fn main() {
 
     let listener = UnixListener::bind(socket_path).expect("Failed to bind socket");
     println!("🔌 Admin socket listening at {}", socket_path);
+
+    fs::set_permissions(socket_path, fs::Permissions::from_mode(0o666)).unwrap();
 
     loop {
         let (stream, _) = listener.accept().await.expect("Socket accept failed");
